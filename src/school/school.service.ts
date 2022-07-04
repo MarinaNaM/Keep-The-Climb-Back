96097fff77828a1817afe04a6f5e11299693411c
Marina <marinadlqs@gmail.com>
2022-07-04 20:17:58 +0200

    Resolve fail import

--- src/school/school.service.ts
+++ src/school/school.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { CreateSchoolDto } from './dto/create-school.dto';
 import { UpdateSchoolDto } from './dto/update-school.dto';
 import { iSchool } from './entities/school.entity';
 
