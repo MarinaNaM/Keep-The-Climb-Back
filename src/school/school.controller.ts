78a96d513c960dc15c6bf193ca871ff9d06ac122
Marina <marinadlqs@gmail.com>
2022-07-04 20:20:09 +0200

    Resolve method create

--- src/school/school.controller.ts
+++ src/school/school.controller.ts
@@ -1,34 +1,42 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+} from '@nestjs/common';
 import { SchoolService } from './school.service';
 import { CreateSchoolDto } from './dto/create-school.dto';
 import { UpdateSchoolDto } from './dto/update-school.dto';
 
 @Controller('school')
 export class SchoolController {
-  constructor(private readonly schoolService: SchoolService) {}
+    constructor(private readonly schoolService: SchoolService) {}
 
-  @Post()
-  create(@Body() createSchoolDto: CreateSchoolDto) {
-    return this.schoolService.create(createSchoolDto);
-  }
+    //   @Post()
+    //   create(@Body() createSchoolDto: CreateSchoolDto) {
+    //     return this.schoolService.create(createSchoolDto);
+    //   }
 
-  @Get()
-  findAll() {
-    return this.schoolService.findAll();
-  }
+    @Get()
+    findAll() {
+        return this.schoolService.findAll();
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.schoolService.findOne(+id);
-  }
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.schoolService.findOne(+id);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSchoolDto: UpdateSchoolDto) {
-    return this.schoolService.update(+id, updateSchoolDto);
-  }
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateSchoolDto: UpdateSchoolDto) {
+        return this.schoolService.update(+id, updateSchoolDto);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.schoolService.remove(+id);
-  }
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.schoolService.remove(+id);
+    }
 }
