5ebd393adf63962bf3342412b107ea5a123b1715
Marina <marinadlqs@gmail.com>
2022-07-04 18:30:27 +0200

    Add school schema

--- dist/school/entities/school.entity.js
+++ dist/school/entities/school.entity.js
@@ -1,7 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.School = void 0;
-class School {
-}
-exports.School = School;
+exports.SchoolSchema = void 0;
+const mongoose_1 = require("mongoose");
+exports.SchoolSchema = new mongoose_1.Schema({
+    name: String,
+    img: String,
+    period: String,
+    localization: {
+        lat: Number,
+        lng: Number,
+    },
+    sectors: { type: mongoose_1.SchemaTypes.ObjectId, ref: 'Section' },
+});
 //# sourceMappingURL=school.entity.js.map
--- src/school/entities/school.entity.ts
+++ src/school/entities/school.entity.ts
@@ -1,1 +1,25 @@
-export class School {}
+import { Schema, SchemaTypes, Types } from 'mongoose';
+import { iRelationField } from '../../interfaces/relation-field';
+
+export interface iSchool {
+    _id?: Types.ObjectId;
+    name: string;
+    img: string;
+    period: string;
+    localization: {
+        lat: number;
+        lng: number;
+    };
+    sectors: iRelationField;
+}
+
+export const SchoolSchema = new Schema({
+    name: String,
+    img: String,
+    period: String,
+    localization: {
+        lat: Number,
+        lng: Number,
+    },
+    sectors: { type: SchemaTypes.ObjectId, ref: 'Section' },
+});
