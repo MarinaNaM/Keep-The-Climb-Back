c794f921ffd8b5cdc431b50772598fcef68e20cb
Marina <marinadlqs@gmail.com>
2022-07-05 20:07:01 +0200

    Update route model

--- src/route/entities/route.entity.ts
+++ src/route/entities/route.entity.ts
@@ -1,32 +1,26 @@
 import { Schema, Types } from 'mongoose';
-import { iRelationFieldVote } from '../../interfaces/relation-field-vote';
 
 export interface iRoute {
     _id?: Types.ObjectId;
     name: string;
-    img: string;
     length: number;
     grade: string;
     voteGrade: [
         {
-            user: iRelationFieldVote;
-            vote: string;
+            user: string;
+            vote: number;
         },
     ];
 }
 
 export const routeSchema = new Schema({
     name: String,
-    img: String,
     lenght: Number,
     grade: String,
     voteGrade: [
         {
-            type: Types.ObjectId,
-            ref: 'User',
-            vote: {
-                type: Number,
-            },
+            user: { type: Types.ObjectId },
+            vote: Number,
         },
     ],
 });
