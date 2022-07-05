cdd8e74419bdd66996a725b6bd308fe97df5922d
Marina <marinadlqs@gmail.com>
2022-07-05 13:38:24 +0200

    Update modules

--- src/route/route.module.ts
+++ src/route/route.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { RouteService } from './route.service';
 import { RouteController } from './route.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { routeSchema } from './entities/route.entity';
+import { userSchema } from 'src/user/entities/user.entity';
 
 @Module({
-  controllers: [RouteController],
-  providers: [RouteService]
+    imports: [
+        MongooseModule.forFeature([
+            { name: 'Route', schema: routeSchema },
+            { name: 'User', schema: userSchema },
+        ]),
+    ],
+    controllers: [RouteController],
+    providers: [RouteService],
 })
 export class RouteModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { userSchema } from './entities/user.entity';
+import { routeSchema } from '../route/entities/route.entity';
+import { AuthService } from '../auth/auth.service';
+import { BcryptService } from '../auth/bcrypt.service';
 
 @Module({
-  controllers: [UserController],
-  providers: [UserService]
+    imports: [
+        MongooseModule.forFeature([
+            { name: 'User', schema: userSchema },
+            { name: 'Route', schema: routeSchema },
+        ]),
+    ],
+    controllers: [UserController],
+    providers: [UserService, AuthService, BcryptService],
 })
 export class UserModule {}
