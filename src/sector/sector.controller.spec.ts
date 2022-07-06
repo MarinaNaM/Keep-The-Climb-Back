c2607cc0f072b0e1d91ac3dbbe52d3f9c31c9659
Marina <marinadlqs@gmail.com>
2022-07-06 18:03:55 +0200

    Add sector controller test

--- src/sector/sector.controller.spec.ts
+++ src/sector/sector.controller.spec.ts
@@ -3,18 +3,70 @@ import { SectorController } from './sector.controller';
 import { SectorService } from './sector.service';
 
 describe('SectorController', () => {
-  let controller: SectorController;
+    let controller: SectorController;
+    let service: SectorService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [SectorController],
-      providers: [SectorService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [SectorController],
+            providers: [
+                {
+                    provide: SectorService,
+                    useValue: {
+                        create: jest.fn(),
+                        findAll: jest.fn(),
+                        findOne: jest.fn(),
+                        update: jest.fn(),
+                        remove: jest.fn(),
+                    },
+                },
+            ],
+        }).compile();
 
-    controller = module.get<SectorController>(SectorController);
-  });
+        controller = module.get<SectorController>(SectorController);
+        service = module.get<SectorService>(SectorService);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
+    describe('When calling controller.create', () => {
+        test('Then service.create should be called', () => {
+            controller.create({
+                name: 'test',
+                img: 'src',
+                hoursSun: 'todo el año',
+                localization: {
+                    lat: 1234,
+                    lng: 1234,
+                },
+                routes: [],
+            });
+            expect(service.create).toHaveBeenCalled();
+        });
+    });
+    describe('When calling controller.findAll', () => {
+        test('Then service.findAll should be called', () => {
+            controller.findAll();
+            expect(service.findAll).toHaveBeenCalled();
+        });
+    });
+    describe('When calling controller.findOne', () => {
+        test('Then service.findOne should be called', () => {
+            controller.findOne('');
+            expect(service.findOne).toHaveBeenCalled();
+        });
+    });
+    describe('When calling controller.update', () => {
+        test('Then service.update should be called', () => {
+            controller.update('', {});
+            expect(service.update).toHaveBeenCalled();
+        });
+    });
+    describe('When calling controller.remove', () => {
+        test('Then service.remove should be called', () => {
+            controller.remove('');
+            expect(service.remove).toHaveBeenCalled();
+        });
+    });
 });
