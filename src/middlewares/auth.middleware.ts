083ae8bd54aae694b59b003f48f8a2e081964afc
Marina <marinadlqs@gmail.com>
2022-07-08 17:04:11 +0200

    Update an exception  in auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -4,7 +4,6 @@ import {
     UnauthorizedException,
 } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
-import { JsonWebTokenError } from 'jsonwebtoken';
 import { AuthService } from '../auth/auth.service';
 
 @Injectable()
@@ -12,10 +11,12 @@ export class AuthMiddleware implements NestMiddleware {
     constructor(private readonly auth: AuthService) {}
     use(req: Request, res: Response, next: NextFunction) {
         const token = req.get('Authorization');
+
         if (!token) throw new UnauthorizedException('Token does not exist');
         const tokenData = this.auth.decodedToken(token.substring(7));
+
         if (typeof tokenData === 'string')
-            throw new JsonWebTokenError('Token invalid');
+            throw new UnauthorizedException('Token invalid 2');
         next();
     }
 }
